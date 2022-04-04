f73e5e80f6e77c17d064f6893fd6318cdc9b1ab8
Your Name <you@example.com>
2022-04-04 16:28:17 +0530

    front end part

---
+++ verify.js
@@ -0,0 +1,70 @@
+let a=document.getElementById('one');
+a.addEventListener("onclick",function(e)
+{
+    e.preventDefault();
+    displayIframeone();
+})
+function displayIframeone()
+{
+    document.getElementById('iframedisplay').innerHTML="<iframe src=\"../form1.html\" height=\"100%\" width=\"100%\" scrolling=\"auto\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\" ></iframe>";
+}
+let b=document.getElementById('two');
+b.addEventListener("onclick",function(e)
+{
+    e.preventDefault();
+    displayIframetwo();
+})
+function displayIframetwo()
+{
+    document.getElementById('iframedisplay').innerHTML="<iframe src=\"../form2.html\" height=\"100%\" width=\"100%\" scrolling=\"auto\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\" ></iframe>";
+}
+let c=document.getElementById('three');
+c.addEventListener("onclick",function(e)
+{
+    e.preventDefault();
+    displayIframethree();
+})
+function displayIframethree()
+{
+    document.getElementById('iframedisplay').innerHTML="<iframe src=\"../form3.html\" height=\"100%\" width=\"100%\" scrolling=\"auto\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\" ></iframe>";
+}
+let d=document.getElementById('four');
+d.addEventListener("onclick",function(e)
+{
+    e.preventDefault();
+    displayIframefour();
+})
+function displayIframefour()
+{
+    document.getElementById('iframedisplay').innerHTML="<iframe src=\"../form4.html\" height=\"100%\" width=\"100%\" scrolling=\"auto\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\" ></iframe>";
+}
+let f=document.getElementById('five');
+f.addEventListener("onclick",function(e)
+{
+    e.preventDefault();
+    displayIframefive();
+})
+function displayIframefive()
+{
+    document.getElementById('iframedisplay').innerHTML="<iframe src=\"../form5.html\" height=\"100%\" width=\"100%\" scrolling=\"auto\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\" ></iframe>";
+}
+let g=document.getElementById('six');
+g.addEventListener("onclick",function(e)
+{
+    e.preventDefault();
+    displayIframesix();
+})
+function displayIframesix()
+{
+    document.getElementById('iframedisplay').innerHTML="<iframe src=\"../form6.html\" height=\"100%\" width=\"100%\" scrolling=\"auto\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\" ></iframe>";
+}
+let h=document.getElementById('seven');
+h.addEventListener("onclick",function(e)
+{
+    e.preventDefault();
+    displayIframeseven();
+})
+function displayIframeseven()
+{
+    document.getElementById('iframedisplay').innerHTML="<iframe src=\"../form7.html\" height=\"100%\" width=\"100%\" scrolling=\"auto\" frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\" ></iframe>";
+}
